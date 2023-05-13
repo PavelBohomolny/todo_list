944d7d0f99458c15a979b233222e81886775b9ea
NexusUA <pbogomolny93@gmal.com>
2023-05-13 14:26:51 +0200

    Header View

--- ToDoList/Views/LoginView.swift
+++ ToDoList/Views/LoginView.swift
@@ -16,7 +16,10 @@ struct LoginView: View {
         NavigationView {
             VStack {
                 // Header
-                HeaderView()
+                HeaderView(title: "To Do List",
+                           subTitle: "Get things done",
+                           angle: 15,
+                           backgroundColor: .pink)
                 
                 // Login Form
                 Form {
@@ -39,6 +42,7 @@ struct LoginView: View {
                     }
                     .padding(8)
                 }
+                .offset(y: -50)
                 
                 // Create Account
                 VStack {
